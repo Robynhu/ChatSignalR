223e922d0b7e60564ac7749d3000a357e63d4a50
Robson Paproski <robsonpski@gmail.com>
2015-11-29 21:31:44 -0200

    Commit inicial

---
+++ ChatSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace ChatSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        //Classe responsável pelo Hub do SignalR
+
+        /// <summary>
+        /// Função chamada pelo cliente para enviar a mensagens aos outros clientes conectados
+        /// </summary>
+        /// <param name="user"> Apelido </param>
+        /// <param name="msg"> Mensagem </param>
+        public void EnviarMsg(string user, string msg)
+        {
+            Clients.All.enviarMensagem(user, msg);
+        }
+    }
+}
---
+++ ChatSignalR/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ChatSignalR.Startup))]
+
+namespace ChatSignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
